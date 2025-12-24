130e0abeaa2417111cb56c3dac94e1ca0d356cb0
Henry Sky <henrysky64@outlook.com>
2025-12-24 16:41:29 +0800

    some fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from PySide6.QtWidgets import QApplication
+from labwindow import LabWindow
 
 
 def main():
-    import pyrealsense2 as rs
-    ctx = rs.context()
-    dev = ctx.query_devices()[0]  # 第一台 D455
-    print('USB type:', dev.get_info(rs.camera_info.usb_type_descriptor))
+    app = QApplication([])
+    window = LabWindow()
+    window.show()
+    app.exec()
 
 if __name__ == '__main__':
     main()
